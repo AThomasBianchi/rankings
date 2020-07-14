95f6ff1ef2664b461bb8c28249ef5263db38cc06
A Thomas Bianchi <atbianchi@gmail.com>
2020-07-14 19:38:58 -0400

    add past replacement level calculations

--- replacementLevel.js
+++ replacementLevel.js
@@ -5,4 +5,25 @@ const replacementLevel = {
   te: 12,
 }
 
+const RL_2019 = {
+  qb: 12,
+  rb: 60,
+  wr: 55,
+  te: 12,
+};
+
+const RL_2018 = {
+  qb: 16,
+  rb: 58,
+  wr: 53,
+  te: 12,
+};
+
+const RL_2017 = {
+  qb: 14,
+  rb: 58,
+  wr: 55,
+  te: 12,
+};
+
 module.exports = replacementLevel;
