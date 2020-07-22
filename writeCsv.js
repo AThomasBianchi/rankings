c9d37c30892b3bc559e41d3de41963c2a6644939
A Thomas Bianchi <atbianchi@gmail.com>
2020-07-21 21:52:35 -0400

    write csv file

--- writeCsv.js
+++ writeCsv.js
@@ -1,8 +1,16 @@
 import Papa from 'papaparse';
+import fs from 'fs';
 import combineAll from './scrape.js';
 
 async function writeCsv() {
   let ranks = await combineAll();
+
+  const csv = Papa.unparse(ranks);
+
+  fs.writeFile('ranks.csv', csv, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  });
 
   for (let i = 0; i < 30; i++) {
     let { player, team, position, vorp } = ranks[i];
