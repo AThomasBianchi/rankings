7b40cd03fcd00c2a3583574785048cf1f03fe17e
A Thomas Bianchi <atbianchi@gmail.com>
2020-08-03 19:02:35 -0400

    resolve promise ranks

--- getRanks.js
+++ getRanks.js
@@ -4,26 +4,31 @@ const fs = require('fs');
 
 const RANKS_URL = 'https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'
 
-const regex = /(?<=\s)[A-Z]+$/m
-
 function getRanks() {
-  rp(RANKS_URL)
-    .then(function (html) {
-      $('#main-container table tbody tr', html).each((i, rank) => {
-        let player = $('.player-label .full-name', rank).text();
-        let team = $('.player-label .grey', rank).text();
-        let ovrrank = parseFloat($('td:nth-of-type(1)', rank).text())
-        let posRank = $('td:nth-of-type(4)', rank).text()
-        console.log(`${player} ${team} ${ovrrank} ${posRank}`)
+  return new Promise((resolve, reject) => {
+    let ranks = [];
+    rp(RANKS_URL)
+      .then(function (html) {
+        $('#main-container table tbody tr', html).each((i, rank) => {
+          let player = $('.player-label .full-name', rank).text();
+          let team = $('.player-label .grey', rank).text();
+          let ovrRank = parseFloat($('td:nth-of-type(1)', rank).text())
+          let posRank = $('td:nth-of-type(4)', rank).text();
+          ranks.push({ player, team, ovrRank, posRank })
+          resolve(ranks);
+        });
+      }).catch((err) => {
+        reject(err);
       });
-    })
+  })
 }
 
-getRanks();
-
-function returnPlayerTeam(player_team, regex) {
-  let index = player_team.search(regex);
-  let player = player_team.slice(0, index).trim();
-  let team = player_team.slice(index).trim();
-  return { player, team };
+async function assignRanks() {
+  const ranks = await getRanks();
+  const json = JSON.parse(fs.readFileSync('./ranks.json'));
+  
+  console.log(ranks);
 }
+
+
+assignRanks();
